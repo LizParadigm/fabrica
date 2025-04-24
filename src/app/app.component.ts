0e1cf4c450d91e5333123ba7f53c5471aa82edeb
LizParadigm <jesss635000@gmail.com>
2025-04-24 09:38:59 -0600

    branch register-page creada y codigo añadido

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AuthPageComponent } from "./modules/auth/page/auth-page/auth-page.component";
+import { RegisterPageComponent } from './modules/auth/page/register-page/register-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AuthPageComponent],
+  imports: [RouterOutlet, AuthPageComponent, RegisterPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
