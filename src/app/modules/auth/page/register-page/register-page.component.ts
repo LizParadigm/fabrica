d4f07106d9d4aba5eee57cfc25ef493986954710
LizParadigm <jesss635000@gmail.com>
2025-04-28 21:19:31 -0600

    register-page con css, falta añadirle api y ver que problemas puedan o no surgir

--- src/app/modules/auth/page/register-page/register-page.component.ts
+++ src/app/modules/auth/page/register-page/register-page.component.ts
@@ -1,12 +1,119 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../../../shared/services/auth.service';
+import { MensajeErrorService } from '../../../../shared/services/mensajeError/mensaje-error.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register-page',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule
+  ],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.css'
 })
-export class RegisterPageComponent {
+export class RegisterPageComponent implements OnInit {
+  // atributos
+  errorNombre: string = "";
+  errorApellidoPaterno: string = "";
+  errorApellidoMaterno: string = "";
+  errorTelefono: string = "";
+  errorDomicilio: string = "";
+  errorPuesto: string = "";
+  errorArea: string = "";
+  errorCorreo: string = "";
+  errorContrasena: string = "";
 
+  buttonAccesible: boolean = true;
+
+  formulario!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private authservice: AuthService,
+    private errores: MensajeErrorService
+  ) {
+  };
+
+  ngOnInit(): void {
+    this.formulario = this.fb.group({
+      nombre: ['', [
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z\s]+$/),
+        Validators.minLength(3),
+      ]],
+
+      apellidoPaterno: ['', [
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z\s]+$/),
+        Validators.minLength(3),
+      ]],
+
+      apellidoMaterno: ['', [
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z\s]+$/),
+        Validators.minLength(3),
+      ]],
+
+      telefono: ['', [
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(10)
+      ]],
+
+      domicilio: ['', [
+        Validators.required,
+      ]],
+
+      puesto: ['', [
+        Validators.required
+      ]],
+
+      area: ['', [
+        Validators.required
+      ]],
+
+      correo: ['', [
+        Validators.required,
+        Validators.email
+      ]],
+
+      contrasena: ['', [
+        Validators.required,
+        Validators.minLength(8)
+      ]]
+    });
+  }
+
+
+  registrar() {
+    this.mensajesError();
+
+    if (this.formulario.valid) {
+      // falta validacion de correo existente o no y ya se procede a enviar el this.formulario al back con el api
+
+    }
+  };
+
+  mensajesError() {
+    this.errorNombre = this.errores.registrarNombre(this.formulario.get('nombre'));
+    this.errorApellidoPaterno = this.errores.registrarApellido(this.formulario.get('apellidoPaterno'));
+    this.errorApellidoMaterno = this.errores.registrarApellido(this.formulario.get('apellidoMaterno'));
+    this.errorTelefono = this.errores.registrarTelefono(this.formulario.get('telefono'));
+    this.errorDomicilio = this.errores.registrarDomicilio(this.formulario.get('domicilio'));
+    this.errorPuesto = this.errores.registrarPuesto(this.formulario.get('puesto'));
+    this.errorArea = this.errores.registrarArea(this.formulario.get('area'));
+    this.errorCorreo = this.errores.registrarCorreo(this.formulario.get('correo'));
+    this.errorContrasena = this.errores.registrarContrasena(this.formulario.get('contrasena'));
+  }
+
+  numeros(event: KeyboardEvent): void {
+    const char = event.key.charCodeAt(0);
+    if (char < 48 || char > 57) {
+      event.preventDefault();
+    }
+  }
 }
---
+++ src/app/shared/services/mensajeError/mensaje-error.service.ts
@@ -0,0 +1,119 @@
+import { Injectable } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MensajeErrorService {
+
+  constructor() { }
+
+  // registrar-page
+  registrarNombre(nombre: AbstractControl | null): string {
+    if (!nombre) return '';
+
+
+    if (nombre.hasError('required')) {
+      return 'Campo necesario.';
+    }
+    else if (nombre.hasError('pattern')) {
+      return 'Solo se permiten letras.';
+    }
+    else if (nombre.hasError('minlength')) {
+      return 'Debe tener mas de 3 letras.';
+    }
+    else {
+      return '';
+    }
+  }
+
+  registrarApellido(apellido: AbstractControl | null): string {
+    if (!apellido) return '';
+
+    if (apellido.hasError('required')) {
+      return 'Campo necesario.';
+    }
+    else if (apellido.hasError('pattern')) {
+      return 'Solo se permiten letras.';
+    }
+    else if (apellido.hasError('minlength')) {
+      return 'Debe tener mas de 3 letras.';
+    } else {
+      return '';
+    }
+  }
+
+  registrarTelefono(telefono: AbstractControl | null): string {
+    if (!telefono) return '';
+    if (telefono.hasError('required')) {
+      return 'Campo necesario.';
+    }
+    else if (telefono.hasError('minlength') || telefono.hasError('maxlength')) {
+      return 'Debe tener 10 numeros.';
+    }
+    else {
+      return '';
+    }
+  }
+
+  registrarDomicilio(domicilio: AbstractControl | null): string {
+    if (!domicilio) return '';
+    if (domicilio.hasError('required')) {
+      return 'Campo necesario.';
+    }
+
+    return '';
+  }
+
+  registrarPuesto(puesto: AbstractControl | null): string {
+    if (!puesto) return '';
+
+    if (puesto.hasError('required')) {
+      return 'Campo necesario.';
+    }
+
+    return '';
+  }
+
+  registrarArea(area: AbstractControl | null): string {
+    if (!area) return '';
+
+    if (area.hasError('required')) {
+      return 'Campo necesario.';
+    }
+
+    return '';
+  }
+
+  registrarCorreo(correo: AbstractControl | null): string {
+    if (!correo) return '';
+
+    if (correo.hasError('required')) {
+      return 'Campo necesario.';
+    }
+    else if (correo.hasError('email')) {
+      return 'Correo invalido.';
+    }
+    // else if (existe){            si esta o no disponible el correo
+    //   return 'Alguien ya registro ese correo.';
+    // }
+
+    return '';
+  }
+
+  registrarContrasena(contrasena: AbstractControl | null): string {
+    if (!contrasena) return '';
+
+    if (contrasena.hasError('required')) {
+      return 'Campo necesario.';
+    }
+    else if (contrasena.hasError('minlength')) {
+      return 'Debe tener mas de 8 caracteres.';
+    }
+    else {
+      return '';
+    }
+  }
+
+
+}
